887820376fcab0c2a49481472e93c2928046d8ca
MAKYU\makyu <289415287@qq.com>
2017-05-18 23:34:59 +0800

    我的安全令页面更新

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,5 +2,18 @@
 //获取应用实例
 var app = getApp()
 Page({
-
+    data:{
+        progress:0
+    },
+    onLoad:function(){
+        var that = this
+        var inter = setInterval(function(){
+            if (that.data.progress >= 100) {
+                clearInterval(inter);
+            }
+            that.setData({
+                progress:that.data.progress + 10
+            });
+        },1000);
+    }
 })
