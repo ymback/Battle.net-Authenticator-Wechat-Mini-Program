dca2c4d78132cfadc958140c843b91d621ec5212
ymback <ymback@sayyoulove.me>
2017-05-22 14:54:29 +0800

    添加安全令页面，读取默认安全令页面
    Signed-off-by: ymback <ymback@sayyoulove.me>

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,39 +1,85 @@
-//index.js
-//获取应用实例
+// index.js
 var app = getApp()
+var second = 0;
+var inter = null;
+var isLoading = false;
 Page({
-    data:{
-        progress:0
-    },
-    onLoad:function(){
-        wx.setNavigationBarTitle({
-            title: '我的安全令'
-        })
+  data: {
+    progress: 0,
+    authCode: ""
+  },
+  onLoad: function () {
+    wx.setNavigationBarTitle({
+      title: '我的安全令'
+    })
 
-        var that = this
-        var inter = setInterval(function(){
-            if (that.data.progress >= 100) {
-                that.setData({
-                    progress:0
-                });
-            }
-            that.setData({
-                progress:that.data.progress + 10
-            });
-        },1000);
-        wx.getStorage({
-          key: 'skey',
-          success: function (res) {
-            console.log(res.data)
-          }
-        })
-        wx.checkSession({
-          success: function(res) 
-          {
-            console.log(res)
-          },
-          fail: function(res) {},
-          complete: function(res) {},
-        })
+    var that = this;
+    wx.getStorage({
+      key: 'skey',
+      success: function (res) {
+        console.log(res.data)
+      }
+    })
+    wx.checkSession({
+      success: function (res) {
+        console.log(res)
+        that.doSync()
+      },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+  doSync: function () {
+    if (isLoading == true) {
+      wx.showToast({
+        title: '请等待刷新',
+        duration: 2000
+      })
+      return;
     }
+    isLoading = true;
+    if (inter != null) {
+      clearInterval(inter);
+      inter = null;
+    }
+    wx.showLoading({
+      title: '刷新中...',
+    })
+    var that = this;
+    let url = "http://myauth.zuzhanghao.com/api/wechat/authDynamicCode";
+    let token = wx.getStorageSync('skey')
+    wx.request({
+      url: url,
+      data: {
+        token_wechat_session_v1: token
+      },
+      header: {},
+      method: 'post',
+      dataType: '',
+      success: function (res) {
+        isLoading = false;
+        wx.hideLoading()
+        if (res.data.code == 200) {
+          that.setData({
+            authCode: res.data.data.dynamicCode
+          })
+          second = res.data.data.usedSecond;
+          inter = setInterval(function () {
+            if (that.data.progress >= 100) {
+              that.setData({
+                progress: 0
+              });
+              that.doSync();
+              return;
+            }
+            second = second + 0.05;
+            that.setData({
+              progress: 100 * (second / 30)
+            });
+          }, 50);
+        }
+      }
+    })
+
+  }
 })
--- pages/login/login.js
+++ pages/login/login.js
@@ -5,14 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
@@ -22,91 +22,99 @@ Page({
     wx.setNavigationBarTitle({
       title: '登录授权中...'
     })
-          wx.login({
+    wx.login({
+      success: function (res) {
+        if (res.code) {
+          let url = "http://myauth.zuzhanghao.com/api/wechat/getSessionToken"
+          wx.request({
+            url: url,
+            data: { code: res.code },
+            header: {},
+            method: 'post',
+            dataType: 'json',
             success: function (res) {
-              if (res.code) {
-                let url = "http://myauth.zuzhanghao.com/api/wechat/getSessionToken"
-                wx.request({
-                  url: url,
-                  data: {code:res.code},
-                  header: {},
-                  method: 'post',
-                  dataType: 'json',
-                  success: function (res) {
-                    wx.setStorageSync(
-                        "skey",
-                        res.data.data.token_wechat_session_v1
-                    )
-                    if (res.data.code == 200) {
-                      wx.redirectTo({
-                        url: '/pages/index/index',
-                        success: function (res) { },
-                        fail: function (res) { },
-                        complete: function (res) { },
-                      })
-                    }
-                    else if (res.data.code == 428)
-                    {
-                      wx.redirectTo({
-                        url: '/pages/bind/bind',
-                        success: function (res) { },
-                        fail: function (res) { },
-                        complete: function (res) { },
-                      })
-                    }else{
-                      wx.showToast({
-                          title: res.data.message,
-                          icon: 'loading'
-                      })
-                    }
-                  }
+              wx.setStorageSync(
+                "skey",
+                res.data.data.token_wechat_session_v1
+              )
+              if (res.data.code == 200) {
+                if (res.data.data.hasAuth === false) {
+                  wx.redirectTo({
+                    url: '/pages/auth/addAuthByServer/addAuthByServer',
+                    success: function (res) { },
+                    fail: function (res) { },
+                    complete: function (res) { },
+                  })
+                } else {
+                  wx.redirectTo({
+                    url: '/pages/index/index',
+                    success: function (res) { },
+                    fail: function (res) { },
+                    complete: function (res) { },
+                  })
+                }
+              }
+              else if (res.data.code == 428) {
+                wx.redirectTo({
+                  url: '/pages/bind/bind',
+                  success: function (res) { },
+                  fail: function (res) { },
+                  complete: function (res) { },
                 })
               } else {
-                console.log('用户信息获取失败' + res.errMsg)
+                wx.showToast({
+                  title: res.data.message,
+                  icon: 'loading'
+                })
               }
             }
           })
+        } else {
+          console.log('用户信息获取失败' + res.errMsg)
+        }
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
