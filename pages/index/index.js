92c25990fd116aa609429e5fdac8a83418af5aa3
ymback <ymback@sayyoulove.me>
2017-05-22 17:04:36 +0800

    战网一键安全令功能
    Signed-off-by: ymback <ymback@sayyoulove.me>

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,7 +2,9 @@
 var app = getApp()
 var second = 0;
 var inter = null;
+var oneButtonAuthInter = null;
 var isLoading = false;
+var hasOneButtonAuthRequest = false;
 Page({
   data: {
     progress: 0,
@@ -10,7 +12,7 @@ Page({
   },
   onLoad: function () {
     wx.setNavigationBarTitle({
-      title: '我的安全令'
+      title: '默认安全令'
     })
 
     var that = this;
@@ -24,6 +26,7 @@ Page({
       success: function (res) {
         console.log(res)
         that.doSync()
+        that.oneButtonAuthLoad()
       },
       fail: function (res) { },
       complete: function (res) { },
@@ -31,10 +34,12 @@ Page({
   },
   doSync: function () {
     if (isLoading == true) {
-      wx.showToast({
-        title: '请等待刷新',
-        duration: 2000
-      })
+      if (!hasOneButtonAuthRequest) {
+        wx.showToast({
+          title: '请等待刷新',
+          duration: 2000
+        })
+      }
       return;
     }
     isLoading = true;
@@ -42,9 +47,11 @@ Page({
       clearInterval(inter);
       inter = null;
     }
-    wx.showLoading({
-      title: '刷新中...',
-    })
+    if (!hasOneButtonAuthRequest) {
+      wx.showLoading({
+        title: '刷新中...',
+      })
+    }
     var that = this;
     let url = "http://myauth.zuzhanghao.com/api/wechat/authDynamicCode";
     let token = wx.getStorageSync('skey')
@@ -57,8 +64,6 @@ Page({
       method: 'post',
       dataType: '',
       success: function (res) {
-        isLoading = false;
-        wx.hideLoading()
         if (res.data.code == 200) {
           that.setData({
             authCode: res.data.data.dynamicCode
@@ -78,8 +83,106 @@ Page({
             });
           }, 50);
         }
+      },
+      complete: function () {
+        isLoading = false;
+        wx.hideLoading()
       }
     })
-
+  },
+  oneButtonAuthLoad: function () {
+    var that = this;
+    let url = "http://myauth.zuzhanghao.com/api/wechat/getOneButtonAuthRequestInfo";
+    let token = wx.getStorageSync('skey')
+    wx.request({
+      url: url,
+      data: {
+        token_wechat_session_v1: token
+      },
+      header: {},
+      method: 'post',
+      dataType: '',
+      success: function (res) {
+        isLoading = false;
+        wx.hideLoading()
+        if (res.data.code == 200) {
+          var json = res.data.data;
+          hasOneButtonAuthRequest = true;
+          wx.showModal({
+            title: '收到一键安全令登录请求',
+            content: "请求信息:" + json.message + "，" + '请求序号:' + json.requestId,
+            cancelText: "拒绝",
+            cancelColor: "#be2a0f",
+            confirmText: "允许",
+            confirmColor: "#4a8102",
+            success: function (res2) {
+              if (res2.confirm) {
+                that.oneButtonAuthSendAgreement(res.data.data, "true");
+              } else if (res2.cancel) {
+                that.oneButtonAuthSendAgreement(res.data.data, "false");
+              }
+            },
+            fail: function () {
+              hasOneButtonAuthRequest = false;
+              setTimeout(() => { that.oneButtonAuthLoad() }, 5000)
+            }
+          });
+          return;
+        }
+        hasOneButtonAuthRequest = false;
+        setTimeout(() => { that.oneButtonAuthLoad() }, 5000)
+      },
+      fail: function () {
+        hasOneButtonAuthRequest = false;
+        setTimeout(() => { that.oneButtonAuthLoad() }, 5000)
+      }
+    })
+  },
+  oneButtonAuthSendAgreement: function (jsondata, agreement) {
+    wx.showLoading({
+      title: '提交中...',
+    })
+    console.log(jsondata.authId);
+    console.log(jsondata.callbackUrl);
+    console.log(jsondata.requestId);
+    console.log(jsondata.time);
+    var that = this;
+    let url = "http://myauth.zuzhanghao.com/api/wechat/commitOneKeyButtonAuthResponse";
+    let token = wx.getStorageSync('skey')
+    wx.request({
+      url: url,
+      data: {
+        token_wechat_session_v1: token,
+        authId: jsondata.authId,
+        callbackUrl: jsondata.callbackUrl,
+        requestId: jsondata.requestId,
+        time: jsondata.time,
+        accept: agreement
+      },
+      header: {},
+      method: 'post',
+      dataType: '',
+      success: function (res) {
+        isLoading = false;
+        wx.hideLoading()
+        wx.showToast({
+          title: res.data.message,
+          icon: 'success',
+          duration: 2000
+        })
+      },
+      fail: function () {
+        wx.hideLoading()
+        wx.showToast({
+          title: "提交失败",
+          icon: 'success',
+          duration: 2000
+        })
+      },
+      complete: function () {
+        hasOneButtonAuthRequest = false;
+        setTimeout(() => { that.oneButtonAuthLoad() }, 5000)
+      }
+    })
   }
 })
--- pages/login/login.js
+++ pages/login/login.js
@@ -22,58 +22,71 @@ Page({
     wx.setNavigationBarTitle({
       title: '登录授权中...'
     })
-    wx.login({
-      success: function (res) {
-        if (res.code) {
-          let url = "http://myauth.zuzhanghao.com/api/wechat/getSessionToken"
-          wx.request({
-            url: url,
-            data: { code: res.code },
-            header: {},
-            method: 'post',
-            dataType: 'json',
-            success: function (res) {
-              wx.setStorageSync(
-                "skey",
-                res.data.data.token_wechat_session_v1
-              )
-              if (res.data.code == 200) {
-                if (res.data.data.hasAuth === false) {
-                  wx.redirectTo({
-                    url: '/pages/auth/addAuthByServer/addAuthByServer',
-                    success: function (res) { },
-                    fail: function (res) { },
-                    complete: function (res) { },
-                  })
-                } else {
-                  wx.redirectTo({
-                    url: '/pages/index/index',
-                    success: function (res) { },
-                    fail: function (res) { },
-                    complete: function (res) { },
-                  })
+    console.log("app.js.login")
+    // wx.checkSession({
+    //   success: function () {
+    //     wx.redirectTo({
+    //       url: '/pages/index/index',
+    //       success: function (res) { },
+    //       fail: function (res) { },
+    //       complete: function (res) { },
+    //     })
+    //   },
+    //   fail: function () {
+        wx.login({
+          success: function (res) {
+            if (res.code) {
+              let url = "http://myauth.zuzhanghao.com/api/wechat/getSessionToken"
+              wx.request({
+                url: url,
+                data: { code: res.code },
+                header: {},
+                method: 'post',
+                dataType: 'json',
+                success: function (res) {
+                  wx.setStorageSync(
+                    "skey",
+                    res.data.data.token_wechat_session_v1
+                  )
+                  if (res.data.code == 200) {
+                    if (res.data.data.hasAuth === false) {
+                      wx.redirectTo({
+                        url: '/pages/auth/addAuthByServer/addAuthByServer',
+                        success: function (res) { },
+                        fail: function (res) { },
+                        complete: function (res) { },
+                      })
+                    } else {
+                      wx.redirectTo({
+                        url: '/pages/index/index',
+                        success: function (res) { },
+                        fail: function (res) { },
+                        complete: function (res) { },
+                      })
+                    }
+                  }
+                  else if (res.data.code == 428) {
+                    wx.redirectTo({
+                      url: '/pages/bind/bind',
+                      success: function (res) { },
+                      fail: function (res) { },
+                      complete: function (res) { },
+                    })
+                  } else {
+                    wx.showToast({
+                      title: res.data.message,
+                      icon: 'loading'
+                    })
+                  }
                 }
-              }
-              else if (res.data.code == 428) {
-                wx.redirectTo({
-                  url: '/pages/bind/bind',
-                  success: function (res) { },
-                  fail: function (res) { },
-                  complete: function (res) { },
-                })
-              } else {
-                wx.showToast({
-                  title: res.data.message,
-                  icon: 'loading'
-                })
-              }
+              })
+            } else {
+              console.log('用户信息获取失败' + res.errMsg)
             }
-          })
-        } else {
-          console.log('用户信息获取失败' + res.errMsg)
-        }
-      }
-    })
+          }
+        })
+      // }
+    // })
   },
 
   /**
